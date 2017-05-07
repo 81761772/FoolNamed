9b5022ff51080230cfd53a44957c09fb57fa4b0b
Yao Chao <81761772@qq.com>
2017-05-07 15:53:39 +0800

    Add files via upload

---
+++ MyComboBox.java
@@ -0,0 +1,37 @@
+package fool_named;
+
+import javax.swing.AbstractListModel;
+import javax.swing.ComboBoxModel;
+
+public class MyComboBox extends AbstractListModel<String> implements ComboBoxModel<String> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String selecteditem = null;
+	String[] test = {"ÄÐ","Å®"};
+	
+	public String getElementAt(int index) {
+		return test[index];
+	}
+	
+	public int getSize() {
+		return test.length;
+	}
+	
+	public void setSelectedItem(Object item) {
+		selecteditem = (String) item;
+	}
+	
+	public Object getSelectedItem() {
+		return selecteditem;
+	}
+	
+	public int getIndex() {
+		for (int i = 0; i < test.length; i++) {
+			if (test[i].equals(getSelectedItem()))
+				return i;
+		}
+		return 0;
+	}
+}
